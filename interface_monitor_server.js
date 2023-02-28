4af1570b9d15f5116480b80d95b8b4b94c190cfe
John Chen <tetlun54@gmail.com>
2023-02-28 13:58:36 +0800

    Updates for Server

--- interface_monitor_server.js
+++ interface_monitor_server.js
@@ -1,34 +1,43 @@
+//npm install express axios
 //run on PM2
 
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 const port = 3000;
+const app = express();
+app.use(express.json());
 
 const baseDirectory = "/root/icdn-bandwidth_interface"; // Replace with your desired base directory
 
 // Endpoint to receive data from the client
-app.post('/bandwidth_interface', async (req, res) => {
+app.post("/bandwidth_interface", async (req, res) => {
   try {
-    const { timestamp, devicename, ddns, txbytes } = req.body; // Assuming your client sends these two fields in the request body
+    console.log(`Received:`+ req);
     
-    //console.log(`Received ${txbytes} bytes from ${devicename}.`);
+    const { stamp, devicename, ddns, txbytes } = req.body;  
     
-    // Create the directory if it doesn't exist
-    const deviceDirectory = path.join(baseDirectory, devicename, timestamp.substring(0, 7));
+    const timestamp = new Date(parseInt(stamp) * 1000);
+    // Get the year, month, and date from the date object
+    const year = timestamp.getFullYear();
+    const month = String(timestamp.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed, so add 1 and pad with 0 if needed
+    const day = String(timestamp.getDate()).padStart(2, "0");
+    const yearMonth = `${year}-${month}`;
+    const yearMonthDate = `${year}-${month}-${day}`;
+
+    // Create the directory if it doesn"t exist
+    const deviceDirectory = path.join(baseDirectory, ddns+"_"+devicename, yearMonth);
     if (!fs.existsSync(deviceDirectory)) {
       fs.mkdirSync(deviceDirectory, { recursive: true });
     }
 
     // Generate the filename based on the timestamp
-    const filename = `${timestamp.substring(0, 10)}_tx.log`;
+    const filename = yearMonthDate+"_tx.log";
     const filePath = path.join(deviceDirectory, filename);
 
     // Append the data to the file
-    fs.appendFileSync(filePath, `${timestamp} ${txbytes}\n`);
+    fs.appendFileSync(filePath, `${timestamp} ${ddns} ${devicename} ${txbytes}\n`);
     
     res.sendStatus(200);
   } catch (error) {
